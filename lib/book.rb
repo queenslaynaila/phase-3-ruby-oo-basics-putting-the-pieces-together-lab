f9d8a01de07bf06873b6d8ea731a5b9212022d6a
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-10-21 06:43:08 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :author, :page_count, :genre 
+    attr_reader :title
+    def initialize(title)
+        @title = title
+      end
+    def turn_page
+        puts  "Flipping the page...wow, you read fast!"
+    end
+end
 
-
+book1= Book.new("And Then There Were None")
+book1.author = "Agatha Christie"
+book1.page_count = 272
+book1.genre = "Mystery"
